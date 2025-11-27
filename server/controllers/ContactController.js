4bb9176bbe76f7bd2b41cf9ea8a3014533a25bac
Gustavo Gomez <gustavo.gomezalv@uanl.edu.mx>
2025-11-27 10:24:48 -0600

    Upload Endpoint Functionality

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -1,5 +1,6 @@
 import csv from 'csv-parser'
 import * as fs from "node:fs";
+import ContactModel from '../models/ContactModel.js';
 
 const readCSV = (path) => {
     return new Promise((resolve, reject) => {
@@ -18,7 +19,9 @@ export const uploadCSV = async (req, res) => {
         const totalContacts = await readCSV(csvPath);
         const usersSet = new Set();
         const rows = []
-
+        const emailCounters = {}
+        const phoneCounters = {}
+        const nameCounters = {}
 
         for (const contact of totalContacts) {
             // Regex Expresion to validate email and Phone
@@ -29,18 +32,52 @@ export const uploadCSV = async (req, res) => {
             const isValidEmail = emailExpresion.test(contact['Correo Electronico']);
             const isValidPhone = phoneExpresion.test(contact['Telefono']);
 
+            // Add Counters
+            if(isValidEmail)
+                emailCounters[contact['Correo Electronico']] = emailCounters[contact['Correo Electronico']] ? emailCounters[contact['Correo Electronico']] + 1 : 1;
+            if(isValidPhone)
+                phoneCounters[contact['Telefono']] = phoneCounters[contact['Telefono']] ? phoneCounters[contact['Telefono']] + 1 : 1;
+            nameCounters[contact['Nombre']] = nameCounters[contact['Nombre']] ? nameCounters[contact['Nombre']] + 1 : 1;
+
+            // Validate whole row
+            if (!usersSet.has(JSON.stringify(contact)))
+                usersSet.add(JSON.stringify(contact));
+            else
+                continue;
 
             rows.push({
                 email: contact['Correo Electronico'],
                 name: contact['Nombre'],
                 phone: contact['Telefono'],
                 isValidEmail,
-                isValidPhone
+                isValidPhone,
+                hasDuplicates: false
             })
-
-
         }
-        res.status(201).json({message: 'Contacts uploaded successfully', contacts: rows});
+
+        rows.forEach(row => {
+            if(emailCounters[row.email] > 1 || phoneCounters[row.phone] > 1 || nameCounters[row.name] > 1) row.hasDuplicates = true;
+        })
+
+
+        await ContactModel.bulkCreate(rows.map((row) =>
+            {
+                return {
+                    email: row.email,
+                    name: row.name,
+                    phone: row.phone,
+                }
+            }
+        ));
+
+        res.status(201).json({
+            message: 'Contacts uploaded successfully',
+            contacts: rows,
+            stats: {
+                totalContacts: rows.length,
+                contactsWithDuplicates: rows.filter(row => row.hasDuplicates).length
+            }
+        });
     } catch (error) {
         res.status(500).json({message: error.message});
     }
